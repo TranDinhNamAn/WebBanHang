f32689a574a47c2ea5ddd4adabeb30b43c9cbc2a
Nguyễn Tới <20130443@st.hcmuaf.edu.vn>
2023-01-05 10:18:10 +0700

    NguyenToi

---
+++ src/main/java/vn/edu/hcmuaf/fit/service/ProductServerADM.java
@@ -0,0 +1,33 @@
+package vn.edu.hcmuaf.fit.service;
+
+import vn.edu.hcmuaf.fit.db.DBConnect;
+import vn.edu.hcmuaf.fit.model.Product;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ProductServerADM {
+    public static List<Product> getData() throws SQLException {
+        List<Product> list = new LinkedList<>();
+        Statement statement = DBConnect.getInstall().get();
+        if (statement != null) {
+            ResultSet rs = statement.executeQuery("select * from product");
+            while (rs.next()) {
+                list.add(new Product(
+                        rs.getString(1),
+                        rs.getString(2),
+                        rs.getDouble(3),
+                        rs.getBoolean(4),
+                        rs.getInt(5)
+
+                ));
+            }
+        } else {
+            System.out.println("No result");
+        }
+        return list;
+    }
+}
