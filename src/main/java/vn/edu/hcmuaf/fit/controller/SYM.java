42d907a12db501a5ba091c4de52fe0a6dcdaa5a1
Trần Đình Nam An <20130196@st.hcmuaf.edu.vn>
2023-01-05 20:39:54 +0700

    Nam An

--- src/main/java/vn/edu/hcmuaf/fit/controller/SYM.java
+++ src/main/java/vn/edu/hcmuaf/fit/controller/SYM.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
-@WebServlet(name = "SYM", value = "/Sym")
+@WebServlet(name = "SYM", value = "/SYM")
 public class SYM extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
